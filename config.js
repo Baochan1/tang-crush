6c195bf47f53dc8ae3c9a0ad8597221b28df6825
Baochan1 <86736571+Baochan1@users.noreply.github.com>
2021-07-01 00:02:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Cc',
-    title: 'Thương vợ nhất trên đời 😙',
-    desc: 'Thương Gấu nhất cuộc đời',
+    btnIntro: 'Click vào đây nhé',
+    title: 'Thương vợ và con nhất trên đời 😙',
+    desc: 'vk thương ck ko, hãy chọn nào',
     btnYes: 'Vẫn cứ là yêu Chồng <33',
     btnNo: 'Không, ck nhé :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'ck biết mà 🥰. Yêu vk nhiều nhiều 😘😘',
     messDesc: 'Tối làm thêm đứa nữa vk nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.facebook.com/truongbao.chan' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
