2b291a2203350d9d67e80686e7214c1edab6b9a7
Baochan1 <86736571+Baochan1@users.noreply.github.com>
2021-06-30 23:52:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Vợ à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnIntro: 'Cc',
+    title: 'Thương vợ nhất trên đời 😙',
+    desc: 'Thương Gấu nhất cuộc đời',
+    btnYes: 'Vẫn cứ là yêu Chồng <33',
+    btnNo: 'Không, ck nhé :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'ck biết mà 🥰. Yêu vk nhiều nhiều 😘😘',
+    messDesc: 'Tối làm thêm đứa nữa vk nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.facebook.com/truongbao.chan' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
